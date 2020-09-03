3bdfd556f91e291e5a79191569ea68bd7f64a758
Evelna Brestavizky <berstovi@post.bgu.ac.il>
2020-09-03 07:40:39 +0300

    colors to log

--- controllers/plates.js
+++ controllers/plates.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 
 const HttpError = require('../models/http-error');
 const Plate = require('../models/plate');
@@ -16,19 +17,22 @@ var options = {
 
 const checkPlate = async (req, res, next) => {
     const imageName = req.imageName ;
-    console.log(`Get new request to check plate, image name: ${imageName}`);
+    console.log(chalk.bgBlueBright(`Get new request to check plate, image name: ${imageName}`));
+
     try {
         const parsedResult = await ocrSpaceApi.parseImageFromLocalFile(req.file.path, options);
         if(!parsedResult.parsedText) {
             throw Error(parsedResult);
         }
-        console.log(`${imageName}: parsed result from ocrSpaceApi: `,{parsedText: parsedResult.parsedText});
+
+        console.log(chalk.blueBright(imageName), ': parsed result from ocrSpaceApi : ', {parsedText: parsedResult.parsedText});
 
         const plateText = parsedResult.parsedText.split('\r\n').join("").split('-').join("").split(' ').join("").split('IL').join("");
-        console.log(`${imageName}: tplateText : ${plateText}`);
+        console.log(chalk.blueBright(imageName), `: plateText: ${plateText}`);
+
 
         const ans = check(plateText);
-        console.log(`${imageName}: result of check : `,ans);
+        console.log(chalk.blueBright(imageName), ': result of check:', ans);
 
         const plate = new Plate({
             imageName,
@@ -39,10 +43,13 @@ const checkPlate = async (req, res, next) => {
 
 
         await plate.save();
-        console.log(`${imageName}: plate saved to DB with id: ${plate._id}`);
+        console.log(chalk.blueBright(imageName), `: plate saved to DB with id: ${plate._id}`);
+
+
         res.send(plate.decision)
     } catch (error) {
-        console.log(`${imageName} \t checkPlate error: \n${error}`);
+        console.log(chalk.redBright(imageName), `: checkPlate error: \n${error}`);
+
         return next(new HttpError('Check plate failed.', 500));
     }
 };
